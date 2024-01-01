127d8595ee963ba8c3392ef3d141a77e2a95f590
moridora <smoridora@gmail.com>
2024-01-02 05:37:31 +0900

    GA追加など

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,10 @@ import '../styles/globals.css'
 import { Header } from '@/components/Header';
 import { Footer } from '@/components/Footer';
 import { ScrollToTopButton } from '@/components/ScrollToTopButton';
+import Script from "next/script";
+import * as gtag from "../lib/gtag";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const theme = createTheme({
   typography: {
@@ -16,9 +20,39 @@ const theme = createTheme({
 });
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouterChange = (url: any) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouterChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouterChange);
+    };
+  }, [router.events]);
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+ 
+           gtag('config', '${gtag.GA_MEASUREMENT_ID}');
+           `,
+        }}
+      />
       <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
         <Header />
         <div style={{ flex: 1 }}>
